b513d4589d2bb8c3f54711ea53c1f53035109c8b
“Pavel <itspavelnosarev@gmail.com>
2021-06-09 15:10:52 -0400

    added class

--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,31 @@
-candy1 = {name: "starburst", price: .99, color: "yellow"}
-candy2 = {name: "twix", price: 1.29, color: "bronze"}
-candy3 = {:name => "snickers", :price => 1.45, :color  => "brown"}
 
-puts "#{candy1[name:] costs #{candy1[price:] while #{candy2[name:]} is  #{candy2[price:]"
-def 
+class Candy
+  def initialize (input_name, price, color)
+    @name = input_name
+    @price = input_price
+    @color = input_color
+  end
+  def  name
+    @name
+  end
+  def color
+    @color
+  end
+  def price
+    @price
+  end
+  def color = (input_color)
+    @color = input_color
+  end
+
+  def print_info
+    puts "The #{name} is #{color} that costs $#{price}."
+end
+
+candy1 = Candy.new("Starburst", 0.99, "yellow")
+candy2 = Candy.new("Twix", 1.25, "bronze")
+candy3 = Candy.new("Snickers",1.49, "brown")
+
+candy1.print_info
+p candy1.price
+p candy3.color
