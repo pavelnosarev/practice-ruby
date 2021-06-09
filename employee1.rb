c431ca7a9394d6b704afe1f0f56b95cc034ab124
“Pavel <itspavelnosarev@gmail.com>
2021-06-09 14:42:35 -0400

    new work

---
+++ employee1.rb
@@ -0,0 +1,9 @@
+# employee1 = ["Majora", "Carter", 80000, true]
+# employee2 = ["Danillo", "Campos", 70000, true]
+
+# puts "#{employee1[0]} #{employee1[1]} makes #{employee1[2]} a year."
+
+employee1 = { :first_name => "Majora", :last_name  => "Carter", :salary" => 80000, "active" => true }
+employee2 = {first_name: Danillo", "last_name " => "Campos", "salary" => 70000, "active" => true }
+
+puts "#{employee1["first_name"]} makes #{employee1["salary"]} a year."
---
+++ store_item.rb
@@ -0,0 +1,6 @@
+candy1 = {name: "starburst", price: .99, color: "yellow"}
+candy2 = {name: "twix", price: 1.29, color: "bronze"}
+candy3 = {:name => "snickers", :price => 1.45, :color  => "brown"}
+
+puts "#{candy1[name:] costs #{candy1[price:] while #{candy2[name:]} is  #{candy2[price:]"
+def 
