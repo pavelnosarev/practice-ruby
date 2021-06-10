24cac6dd7c059d419db6bdcad747cb6b6900fe5c
“Pavel <itspavelnosarev@gmail.com>
2021-06-10 14:35:40 -0400

    updated with super

--- inheritance_ex.rb
+++ inheritance_ex.rb
@@ -1,4 +1,5 @@
-class Car
+class  Vehicle
+  attr_accessor :brake :accelerate :turn :honk_horn
   def initialize
     @speed = 0
     @direction = "north"
@@ -15,18 +16,27 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
-
+class Car < Vehicle
+  def initialize(input_options)
+    super
+    @fuel = input_options[:fuel]
+    @make = input_options[:make]
+    @model = input_options[:model]
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike < Car
-  def ring_bell
+class Bike < Vehicle
+  def ring_bell(input_options)
+    super
+    @type= input_options[:type]
+    @weight = input_options[:weight]
+    @model = input_options[:height]
     puts "Ring ring!"
   end
 end
-
+car = Car.new(fuel: "gasoline")
 p car.accelerate
 p bike.accelerate
 
